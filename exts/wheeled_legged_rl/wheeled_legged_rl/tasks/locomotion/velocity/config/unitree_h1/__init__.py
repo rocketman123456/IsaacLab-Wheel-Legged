80a067dba0b2351e2abdce38cc437e2f9809aa05
Zhang Yf <759094438@qq.com>
2025-01-03 13:17:13 +0800

    [ADD] add humanoid and quadruped example from robot_lab

---
+++ exts/wheeled_legged_rl/wheeled_legged_rl/tasks/locomotion/velocity/config/unitree_h1/__init__.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="RobotLab-Isaac-Velocity-Rough-Unitree-H1-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:UnitreeH1RoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeH1RoughPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="RobotLab-Isaac-Velocity-Flat-Unitree-H1-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:UnitreeH1FlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeH1FlatPPORunnerCfg",
+    },
+)
